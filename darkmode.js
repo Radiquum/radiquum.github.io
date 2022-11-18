04669632c82336b9e43cdb070e1e5260ec9f4ec7
Radiquum <kentai.waah@gmail.com>
2022-11-18 16:59:08 +0500

    grid instead of flex for links

--- darkmode.js
+++ darkmode.js
@@ -5,16 +5,18 @@ const darkModeToggle = document.querySelector('.dark-mode-toggle');
 
 const enableDarkMode = () => {
   document.body.classList.add('darkmode');
-  document.getElementById('light').classList.add('disable')
-  document.getElementById('dark').classList.remove('disable')
+
+  document.getElementById('sun').style.visibility='visible';
+  document.getElementById('moon').style.visibility='hidden';
 
   localStorage.setItem('darkMode', 'enabled');
 }
 
 const disableDarkMode = () => {
   document.body.classList.remove('darkmode');
-  document.getElementById('dark').classList.add('disable')
-  document.getElementById('light').classList.remove('disable')
+
+  document.getElementById('sun').style.visibility='hidden';
+  document.getElementById('moon').style.visibility='visible';
 
   localStorage.setItem('darkMode', null);
 }
