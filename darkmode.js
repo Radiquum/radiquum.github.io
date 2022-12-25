640d86bebc481533c0d9994c00152a520602f8dd
Kentai Radiquum <kentai.waah@gmail.com>
2022-12-25 18:45:28 +0500

    last.fm widget fixes
    [=] fix lastfm widget caching issue
    [=] fix lastfm mobile sizing
    [=] minor changes

--- darkmode.js
+++ darkmode.js
@@ -3,12 +3,14 @@ let darkMode = localStorage.getItem('darkMode');
 
 const darkModeToggle = document.querySelector('.dark-mode-toggle');
 
+let random_nmb = Math.floor(Math.random() * 655255) + 1; 
+
 const enableDarkMode = () => {
   document.body.classList.add('darkmode');
 
   document.getElementById('sun').style.visibility='visible';
   document.getElementById('moon').style.visibility='hidden';
-  document.getElementById('music-badge').src="https://img.shields.io/endpoint?color=ffffff&url=https://lastfm-last-played.biancarosa.com.br/Radiquum/latest-song?format=shields.io&style=for-the-badge&label=%20";
+  document.getElementById('music-badge').src="https://img.shields.io/endpoint?color=ffffff&url=https://lastfm-last-played.biancarosa.com.br/Radiquum/latest-song?format=shields.io&style=for-the-badge&label=%20&nocache=" + random_nmb;
 
   localStorage.setItem('darkMode', 'enabled');
 }
@@ -18,7 +20,7 @@ const disableDarkMode = () => {
 
   document.getElementById('sun').style.visibility='hidden';
   document.getElementById('moon').style.visibility='visible';
-  document.getElementById('music-badge').src="https://img.shields.io/endpoint?color=ffaecc&url=https://lastfm-last-played.biancarosa.com.br/Radiquum/latest-song?format=shields.io&style=for-the-badge&label=%20";
+  document.getElementById('music-badge').src="https://img.shields.io/endpoint?color=ffaecc&url=https://lastfm-last-played.biancarosa.com.br/Radiquum/latest-song?format=shields.io&style=for-the-badge&label=%20&nocache=" + random_nmb;
 
   localStorage.setItem('darkMode', null);
 }
