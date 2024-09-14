b09a84afbef6ceda8ff3955c5af17c06aed775c1
Kentai Radiquum <kentai.waah@gmail.com>
2024-09-14 13:54:03 +0500

    feat: add landing

--- static/script.js
+++ static/script.js
@@ -17,7 +17,46 @@ function updatePlayingTrack() {
     });
 }
 
+function getScrollPosition () {
+  const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    const windowScroll = document.documentElement.scrollTop;
+    const scrolled = (windowScroll / height) * 100;
+    return Math.floor(scrolled)
+}
+
 window.onload = () => {
   updatePlayingTrack();
   setInterval(updatePlayingTrack, 360000);
+}
+const header = document.getElementById("header")
+let last_Y_pos = 0
+let header_opacity = 0
+
+window.onscroll = () => {
+  let scrollPosition = getScrollPosition()
+  console.log(last_Y_pos, window.scrollY, scrollPosition, header_opacity)
+
+  if (scrollPosition < 20) {
+    header.style.display = "none"
+  } else {
+    header.style.display = "block"
+  }
+
+
+  if ((window.scrollY > last_Y_pos) && (scrollPosition > 40)) {
+    header_opacity += 0.1
+  } else if ((window.scrollY < last_Y_pos) && (scrollPosition < 40)) (
+    header_opacity -= 0.1
+  )
+  last_Y_pos = window.scrollY
+
+  if (header_opacity > 1) {
+    header_opacity = 1
+  } else if (header_opacity < 0) {
+    header_opacity = 0
+  }
+
+  header.style.setProperty("--header-opacity", `${header_opacity.toFixed(2)}`)
 }
