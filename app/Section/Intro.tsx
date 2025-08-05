bd2388aaa5bdac646dbf3578167b71852dc61efb
Radiquum <kentai.waah@gmail.com>
2025-08-05 23:28:44 +0500

    feat: add backdrop behind intro text

--- app/Section/Intro.tsx
+++ app/Section/Intro.tsx
@@ -10,7 +10,8 @@ export const Intro = () => {
   }, []);
 
   return (
-    <div>
+    <div className="relative">
+      <div className="absolute -bottom-24 left-0 right-0 top-0 -z-10 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:16px_16px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_90%,transparent_100%)]"></div>
       <div className="flex flex-col mx-auto max-w-fit md:items-center justify-center h-fit py-16 gap-4">
         <h1 className="text-4xl md:text-8xl font-medium">Hi, I&apos;m <span className="bg-clip-text bg-gradient-to-br from-[#FFB1CD] via-[#C8E8FE] to-white text-transparent font-bold">Radiquum</span></h1>
         <div className="flex flex-col lg:flex-row lg:gap-8 overflow-hidden">
