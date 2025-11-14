b0d51fe87ca0bec3b0e8ae71df5fcc569422a86a
Radiquum <kentai.waah@gmail.com>
2025-11-14 17:19:03 +0500

    feat: add avatar to intro

--- app/Section/Intro.tsx
+++ app/Section/Intro.tsx
@@ -13,6 +13,7 @@ export const Intro = () => {
     <div className="relative">
       <div className="absolute -bottom-24 left-0 right-0 top-0 -z-10 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:16px_16px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_90%,transparent_100%)]"></div>
       <div className="flex flex-col mx-auto max-w-fit md:items-center justify-center h-fit py-16 gap-4">
+        <img src="/images/avatar.png" alt="" className="w-48 h-48 rounded-full border border-white/15"></img>
         <h1 className="text-4xl md:text-8xl font-medium">Hi, I&apos;m <span className="bg-clip-text bg-gradient-to-br from-[#FFB1CD] via-[#C8E8FE] to-white text-transparent font-bold">Radiquum</span></h1>
         <div className="flex flex-col lg:flex-row lg:gap-8 overflow-hidden">
           <p className={`text-2xl md:text-4xl text-[#C8E8FE] ${isAnimated ? "translate-y-0 opacity-100" : "translate-y-full opacity-0"} transition-[transform_opacity] duration-1000 ease-out`}>Developer</p>
